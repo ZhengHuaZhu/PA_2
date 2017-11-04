e6d71c86d213f1e41436ca87f07cd1b40acc8f03
ZhengHuaZhu <zzh.montreal.qc@gmail>
2017-11-04 11:23:12 -0400

    Semaphore class modified; added mutexes to StackManager but not finished yet

---
+++ src/charStackExceptions/CharStackEmptyException.java
@@ -0,0 +1,7 @@
+package charStackExceptions;
+
+public class CharStackEmptyException extends Exception {
+	public CharStackEmptyException() {
+		super("Char Stack is empty.");
+	}
+}
---
+++ src/charStackExceptions/CharStackInvalidAccessException.java
@@ -0,0 +1,8 @@
+package charStackExceptions;
+
+public class CharStackInvalidAccessException extends Exception {
+	// Fill it up yourself
+	public CharStackInvalidAccessException() {
+		super("Invalid access position specified.");
+	}
+}
